db68db6936adf165f8980124a2e4c6d816fe3526
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:50:11 +0100

    fixed with ESlint

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,25 @@
-export function taskFirst () {
+/**
+ * Returns the task as a constant string.
+ * @return {string} The task string.
+ */
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-export function getLast () {
+/**
+ * Returns a string indicating that something is okay.
+ * @return {string} The "is okay" string.
+ */
+export function getLast() {
   return ' is okay';
 }
 
-export function taskNext () {
+/**
+ * Combines two strings and returns the result.
+ * @return {string} The combined string.
+ */
+export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
