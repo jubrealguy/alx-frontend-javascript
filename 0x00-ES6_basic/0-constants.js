c0343b379f979a8e6b978bc16133a065f9e69b64
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:52:21 +0100

    unfix

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,24 +1,12 @@
-/**
- * Returns the task as a constant string.
- * @return {string} The task string.
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
- * Returns a string indicating that something is okay.
- * @return {string} The "is okay" string.
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * Combines two strings and returns the result.
- * @return {string} The combined string.
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
