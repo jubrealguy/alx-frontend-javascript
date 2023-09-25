c1c2b77f50be9650c0bb57924452e86bed4761fa
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:55:05 +0100

    fixed with ESlint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,10 @@
+/**
+ * This function returns an array containing two boolean values.
+ *
+ * @param {boolean} trueOrFalse - A boolean
+ * value that determines the return values.
+ * @return {Array<boolean>} An array containing two boolean values.
+ */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
