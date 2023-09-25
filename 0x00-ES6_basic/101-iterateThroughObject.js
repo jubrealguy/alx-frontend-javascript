90f856925c67178788162c646b810796905bc42e
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:32:57 +0100

    fixed with ESLint

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
-export default function iterateThroughObject (reportWithIterator) {
+/**
+ * Iterates through an iterable and joins its elements with a separator.
+ *
+ * @param {Iterable} reportWithIterator - The iterable to iterate through.
+ * @return {string} A string containing the joined elements.
+ */
+export default function iterateThroughObject(reportWithIterator) {
   return [...reportWithIterator].join(' | ');
 }
