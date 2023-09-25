aec21ca99dc18a6282688f14a2bef8a17571f7e2
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:36:34 +0100

    fixed with ESLint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,10 @@
+/**
+ * Creates an array of employees by iterating through the
+ * provided report object.
+ *
+ * @param {Object} report - The report object containing employee information.
+ * @return {Array} An array containing all employees.
+ */
 export default function createIteratorObject(report) {
   let allEmployees = [];
   for (const item of Object.values(report.allEmployees)) {
