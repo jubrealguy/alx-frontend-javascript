ad64f58a0723e62831e9eda6d9a8cf46cda468eb
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 15:16:11 +0100

    return length of argument

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,35 +1,26 @@
 module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: ['jest'],
-  rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement'
-    ]
-  },
-  overrides: [
+  'extends': 'google',
+  'overrides': [
     {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js'
-    }
-  ]
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
 };
