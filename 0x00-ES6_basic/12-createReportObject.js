d7e9ecbccdce2ac3b43704085cc30d1a955299d0
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-25 20:39:50 +0100

    fixed with ESLint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,17 @@
+/**
+ * Create a report object from a list of employees.
+ *
+ * @param {Object} employeesList - An object mapping
+ * departments to lists of employees.
+ * @return {Object} A report object containing all employees
+ * and a method to get the number of departments.
+ */
 export default function createReportObject(employeesList) {
-	return {
-		allEmployees: {
-			...employeesList,
-		},
-		getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
-	};
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) =>
+      Object.keys(employeesList).length,
+  };
 }
