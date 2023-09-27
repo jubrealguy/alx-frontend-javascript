c6aae0572474d486a3fb245c24c89e2f71badfdd
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-27 17:00:02 +0100

    promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,13 @@ import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [signUpUser(), uploadPhoto()]
+    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
     return Promise.allSettled(promises)
     .then((value) => {
-        value.state;
-    })
+        const returnStatus = [];
+        for (const val of value) {
+            returnStatus.push({ status: val.status, value: val.value || val.reason });
+        }
+        return returnStatus;
+    });
 };
