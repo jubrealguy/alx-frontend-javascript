645273e11e294bc14b8a4f39af24e6db624100d7
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-27 22:54:00 +0100

    eslint fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-    const returnStatus = [];
-    return Promise.allSettled(promises)
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const returnStatus = [];
+  return Promise.allSettled(promises)
     .then((value) => {
-        for (const val of value) {
-            returnStatus.push({ status: val.status, value: val.value || val.reason });
-        }
-        return returnStatus;
+      for (const val of value) {
+        returnStatus.push({ status: val.status, value: val.value || val.reason });
+      }
+      return returnStatus;
     })
-    .catch((error) => {return Error()})
-};
+    .catch(() => Error());
+}
