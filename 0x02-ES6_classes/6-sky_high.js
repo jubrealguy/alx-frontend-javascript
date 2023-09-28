633a9f82268d91a35394e0ddf6de5ff67840ef83
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-28 19:35:26 +0100

    inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,9 +2,6 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof sqft !== 'number') throw new Error('sqft must be a number');
-    else if (typeof floors !== 'number') throw new Error('floors must be a number');
-
     super(sqft);
     this._floors = floors;
   }
