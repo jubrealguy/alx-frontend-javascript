51c26380200dacf85d8577ba76c7ce086f22c5c6
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-28 19:49:35 +0100

    string representation

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,15 +1,15 @@
 export default class Airport {
-    constructor(name, code) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        } else if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this._name = name;
-        this._code = code;
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    this._name = name;
+    this._code = code;
+  }
 
-    toString() {
-        return `${this._name} ${this._code}`;
-    }
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
