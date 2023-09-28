3639654f3b7ef0c9b31f46f88dde24ae027edec4
jubrealguy <whizkidjubrealguy@gmail.com>
2023-09-28 15:37:28 +0100

    made it simple

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const sizes = [19, 20, 35];
-  const rooms = [];
-  for (const size of sizes) {
-    const room = new ClassRoom(size);
-    rooms.push(room);
-  }
-  return rooms;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(24),
+  ]
 }
