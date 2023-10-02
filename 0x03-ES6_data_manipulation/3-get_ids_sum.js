366f5874ba550453f21842a8a2a47290d121b2ac
jubrealguy <whizkidjubrealguy@gmail.com>
2023-10-02 20:57:28 +0100

    eslint fixed

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,3 @@
 export default function getStudentIdsSum(arr) {
-    if (!Array.isArray(arr)) {
-        return
-    }
-    const newArr = []
-    arr.map((item) => {
-        newArr.push(item.id)
-    })
-    return newArr.reduce((total, current) => {return total + current}, 0)
+  return arr.reduce((total, current) => total + current.id, 0);
 }
