e3525d6b0287655536fc122c5f0b81167f6f1341
jubrealguy <whizkidjubrealguy@gmail.com>
2023-10-02 22:41:30 +0100

    updated 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,13 @@
 export default function cleanSet(set, startString) {
   if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
-        return ''
+    return '';
+  }
+
+  const newArr = [];
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      newArr.push(item.slice(startString.length));
     }
-    const arr = [...set]
-    const newArr = []
-    arr.map((item) => {
-        if (item && item.startsWith(startString)) {
-            newArr.push(item.slice(startString.length))
-        }
-    })
-    return newArr.join('-');
-  ;
+  }
+  return newArr.join('-');
 }
